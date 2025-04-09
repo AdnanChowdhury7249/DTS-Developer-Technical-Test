c7fa223333031108dcd66b4dba7b760ba093eeb8
AdnanChowdhury7249 <adnanchowdhury7249@gmail.com>
2025-04-09 22:48:00 +0100

    implemented the routers for the tasks

--- routes/tasksRouter.js
+++ routes/tasksRouter.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const {
   getAllTasks,
+  postCreateTask,
+  getTasksByTitle,
+  handleDeleteTask,
+  putUpdateTask,
 } = require('../controllers/taskControllers');
 
 const router = express.Router();
 
 router.get('/', getAllTasks);
+router.post('/', postCreateTask);
+router.get('/search/title', getTasksByTitle);
+router.delete('/:id', handleDeleteTask);
+router.put('/:id', putUpdateTask);
 
 module.exports = router;
